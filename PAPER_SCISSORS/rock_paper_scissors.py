a879dc55d4d72d68aa4f86001f3914ab5eff285b
BSadioDiallo <102491268+BSadioDiallo@users.noreply.github.com>
2022-11-19 13:16:31 +0000

    Rock paper scissors game
    The main file

---
+++ PAPER_SCISSORS/rock_paper_scissors.py
@@ -0,0 +1,69 @@
+from os import name as os_name, system
+import rock_paper_scissors_helper as helper
+from random import randint
+
+windows = 'nt'
+linux = 'posix'
+def clear_screen(os_name):
+    print(os_name)
+    if os_name == windows:
+        system('cls')
+    elif os_name == linux:
+        system('clear')
+
+def user_and_machine():
+    print('USER VS MACHINE')
+    score_1, score_2 = 0 , 0
+    helper.display_state(score_1,score_2)
+    clear_screen(os_name)
+    while score_1 != 3 and score_2 != 3 and score_1 != '!':
+        player_1 = helper.user_input(helper.PLAYER1)
+        player_2 = randint(1,3)
+        if player_1 != helper.STOP:
+            clear_screen(os_name)
+            round_result = helper.the_round(player_1, player_2)
+            score_1, score_2 = helper.point_distributor(round_result, score_1, score_2)
+            helper.display_state(score_1, score_2)
+        else:
+            return
+    clear_screen(os_name)
+    if score_1 == 3:
+        print('You are the winner')
+    else:
+        print('You lose!')
+   
+def user_and_user():
+    print('USER VS USER')
+    score_1, score_2 = 0 , 0
+    helper.display_state(score_1,score_2)
+    clear_screen(os_name)
+    while score_1 != 3 and score_2 != 3:
+        player_1 = helper.user_input(helper.PLAYER1)
+        clear_screen(os_name)
+        player_2 = helper.user_input(helper.PLAYER2)
+        if helper.STOP not in [ player_1, player_2]:
+            clear_screen(os_name)
+            round_result = helper.the_round(player_1, player_2)
+            score_1, score_2 = helper.point_distributor(round_result, score_1, score_2)
+            helper.display_state(score_1, score_2)
+            clear_screen(os_name)
+        else:
+            return
+    if score_1 > score_2:
+        print(f'{helper.PLAYER1} You are the winner ')
+    else:
+        print(f'{helper.PLAYER2} You are the winner ')
+
+def main():
+    mode = helper.menu()
+    if mode == helper.MACHINE:
+        clear_screen(os_name)
+        user_and_machine()
+    elif mode == helper.USER:
+        clear_screen(os_name)
+        user_and_user()
+    
+    print('GOOD BYE')
+    
+if __name__ == '__main__':
+    main()
